19c41494ede807ab7191c40110ed7241c658c543
vikash2696@gmail.com <vikash2696@gmail.com>
2022-02-05 16:05:40 +0530

    mongoDB connection and crud application

--- app.js
+++ app.js
@@ -1,36 +1,91 @@
 const express =  require('express');
 var app = express();
-let data = require('./data.json');
-console.log('data in start', data);
-const fs = require('fs');
+const mongoose = require("mongoose");
 app.use(express.json()); //middleware
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+const URL = process.env.url;
+
+
+mongoose.connect(URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+}).then(() => {
+    console.log('Database conneted');
+})
+.catch((err) => {
+    console.log(err);
+});
+
+const userSchema = new mongoose.Schema({
+    userID : {type : Number, unique :true, required : true},
+    name : String,
+    phone : Number
+},
+{versionKey : false});
+
+//Model Object in collection users
+const userModel = mongoose.model('Users',userSchema);
 
 app.get('/',(req,res) => {
     res.send('Welcome to node application!');
-})
-app.get('/users', function(req, res) {
-    res.status(200).send(data);
 });
-app.post('/user/add', (req, res) => {
-    console.log(req.body);
-    if(data && data.length > 0) {
-        data.push(req.body);
-    }else {
-        console.log('else part');
-        data = [req.body];
-    }
 
-    fs.writeFile('data.json',JSON.stringify(data), (err) => {
+app.get('/users', function(req, res) {
+    userModel.find({},(err, data) => {
+        if(!err) {
+            console.log(data);
+            res.status(200).send({status : 'ok', data});
+        }else {
+            console.log(err);
+            res.status(400).send('Something went wrong');
+        }
+    })
+});
+app.post('/user/add', async (req, res) => {
+    console.log(req.body);
+    const UserData = new userModel(req.body);
+    await UserData.save().then((result) => {
+        console.log('data saved succesfully');
+        res.status(200).send({status : 'ok', result});
+    }).catch((err) => {
         if(err) {
-            throw err
-        };
-        console.log('Record created sucessfully');
-    });
-    res.status(200).send({msg : 'OK', data});
+            res.status(400).send('Something went wrong');
+        }
+    })
     
 });
 
+app.patch('/user/update/:userID',async (req,res) => {
+    const userID = req.params.userID;
+    console.log(userID);
+    await userModel.findOneAndUpdate({userID}, req.body,{upsert : false})
+    .then((result) => {
+        console.log('Data updated');
+        res.status(200).send({status : 'ok', result});
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(400).send('something went wrong');
+    });
+    console.log('kdhgkhdkhsds');
+});
+app.delete('/user/delete/:userID' , (req,res) => {
+    console.log('in delete');
+    const userID = req.params.userID;
+    userModel.deleteOne({userID})
+    .then(() => {
+        res.status(200).send({status : 'ok', msg : 'deleted'});
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(400).send('something went wrong');
+    });
+});
 
-app.listen(3000, function () {
+
+app.listen(PORT, function () {
     console.log('Application is running on port:  3000');
 })
